c04bdd15c80200232d7a7527ce0a7b7a98b3fe41
dgesin <derekgesin@gmail.com>
2023-11-01 14:51:14 -0400

    Makes Home collaboration image responsive.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -38,12 +38,14 @@
                     <p>Visualize the transformation of traditional, paper-laden procedures into streamlined, digital processes, accelerating the IRS's efficiency and ensuring every taxpayer's dollar is put to optimal use.</p>
                     <p>Envision an IRS where the technological backbone is robust, adaptable, and fortified, guaranteeing quicker and more dependable services.</p>
                 </div>
+				<div class="col-md-6 py-md-5">
+					<img class="img-fluid" src="@/assets/couple-completing-taxes.jpg" id="tax-forms-img" />
+				</div>
             </div>
-			<img src="@/assets/bg-triangle-pink.png" id="tax-forms-img" />
         </div>
 		</div>
 		
-        <div class="py-5 bg-mint-green">
+        <div class="py-5 bg-light-sky-blue">
             <div class="container">
 				<div class="container row" align="center">
 	                <div class="col-md-4 justify-content-md-start text-center">
@@ -64,44 +66,44 @@
         
         <div class="bg-white">
             <div class="container py-5">
-                <h1 class="text-deep-teal pt-4">United in Purpose.</h1>
+                <h1 class="text-deep-teal pt-4">United in Purpose</h1>
                 <p>The prevailing "large-scale" contractor approach often falls short in fostering the collaboration, transparency, and efficiency that nimble, progressive firms thrive on. Our ambition is unity over rivalry. We aim to unite visionary small to midsize enterprises, all driven by the shared objective of enhancing the IRS's operations for the benefit of every American taxpayer.</p>
-                <h3 class="text-deep-teal mt-5">Our Pledge for a Digital IRS.</h3>
-                <div class="row py-0">
-                    <div class="col-md-4">
-                        <h6 class="text-deep-teal mt-1 mb-1">Mission over Margin</h6>
-                        <p>Our drive is the purpose, not the profit.</p> 	
-                    </div>
-                    <div class="col-md-4">
-                        <h6 class="text-deep-teal mt-1 mb-1">Collaboration over Recognition</h6>
-                        <p>Every milestone achieved propels us towards a digitally advanced IRS.</p>
-                    </div>
-                    <div class="col-md-4">
-                        <h6 class="text-deep-teal mt-1 mb-1">Results over Efforts</h6>
-                        <p>It's the impact that counts, not just the journey.</p>
-                    </div>
-                </div>
-                <div class="row py-0">
-                    <div class="col-md-4">
-                        <h6 class="text-deep-teal mt-1 mb-1">Taxpayers over Bureaucracy</h6>
-                        <p>Unless we cater to taxpayer needs, our objectives will remain unfulfilled.</p>
-                    </div>
-                    <div class="col-md-4">
-                        <h6 class="text-deep-teal mt-1 mb-1">Progress over Preservation</h6>
-                        <p>Innovation emerges from embracing the unknown.</p>
-                    </div>
-                    <div class="col-md-4">
-                        <h6 class="text-deep-teal mt-1 mb-1">Execution over Discussion</h6>
-                        <p>True excellence is when taxpayers affirm its perfection.</p>
-                    </div>
-                </div>
+				<div class="bg-mint-green p-3 mt-5">
+	                <h3 class="text-deep-teal mb-3">Our Pledge for a Digital IRS</h3>
+	                <div class="row py-0">
+	                    <div class="col-md-4">
+	                        <h6 class="text-deep-teal mt-1 mb-1">Mission over Margin</h6>
+	                        <p>Our drive is the purpose, not the profit.</p> 	
+	                    </div>
+	                    <div class="col-md-4">
+	                        <h6 class="text-deep-teal mt-1 mb-1">Collaboration over Recognition</h6>
+	                        <p>Every milestone achieved propels us towards a digitally advanced IRS.</p>
+	                    </div>
+	                    <div class="col-md-4">
+	                        <h6 class="text-deep-teal mt-1 mb-1">Results over Efforts</h6>
+	                        <p>It's the impact that counts, not just the journey.</p>
+	                    </div>
+	                </div>
+	                <div class="row py-0">
+	                    <div class="col-md-4">
+	                        <h6 class="text-deep-teal mt-1 mb-1">Taxpayers over Bureaucracy</h6>
+	                        <p>Unless we cater to taxpayer needs, our objectives will remain unfulfilled.</p>
+	                    </div>
+	                    <div class="col-md-4">
+	                        <h6 class="text-deep-teal mt-1 mb-1">Progress over Preservation</h6>
+	                        <p>Innovation emerges from embracing the unknown.</p>
+	                    </div>
+	                    <div class="col-md-4">
+	                        <h6 class="text-deep-teal mt-1 mb-1">Execution over Discussion</h6>
+	                        <p>True excellence is when taxpayers affirm its perfection.</p>
+	                    </div>
+	                </div>
+				</div>
             </div>
         </div>
         
-        <div class="bg-parallax tint-light-blue">
-            <parallax :speed-factor=".1" sectionClass="parallax-outside-cover" containerClass="parallax-inside">
-                <img src="@/assets/collaboration.jpg" />
-            </parallax>
+        <div class="tint-light-blue img-short">
+            <img src="@/assets/collaboration.jpg" class="img-fluid"/>
         </div>
 
     </section>
