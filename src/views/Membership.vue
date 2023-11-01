31f1f1472748433f42b522e694149c2c18c601d1
dgesin <derekgesin@gmail.com>
2023-11-01 16:16:00 -0400

    Makes Membership page responsive.

--- src/views/Membership.vue
+++ src/views/Membership.vue
@@ -1,24 +1,25 @@
 <template>
     <section id="membership">	        
-		<Nav/>
-		<div class="banner-text jumbotron">
-				<h1 class="text-white vertical-center text-center">Join the Coalition</h1>
-		        <div class="banner-video-bg">
-		            <video autoplay loop muted>
-						<source src="@/assets/team-coalition.mp4" type="video/webm"/>
-						<source src="@/assets/team-coalition.mp4" type="video/mp4"/>
-					</video>
-		        </div>
+		<div class="banner-text">
+			<h1 class="text-white vertical-center text-center">Join the Coalition</h1>
+	        <div class="banner-video-bg">
+	            <video autoplay loop muted>
+					<source src="@/assets/team-coalition.mp4" type="video/webm"/>
+					<source src="@/assets/team-coalition.mp4" type="video/mp4"/>
+				</video>
+	        </div>
 		</div>	  
 	
         <div class="alert-full-width">
-            <h2 class="h4"><b>Now Accepting Applications!</b></h2>
-            <p>We are now accepting applications for membership. Prior to filling out an application, please review the guidelines below:</p>
-			<a class=”link” href="mailto:info@strateratech.com" target="_blank">Email Us for More Info</a>            
+			<div class="container">
+	            <h2 class="h4"><b>Now Accepting Applications!</b></h2>
+	            <p>We are now accepting applications for membership. Prior to filling out an application, please review the guidelines below:</p>
+				<a class=”link” href="mailto:info@strateratech.com" target="_blank">Email Us for More Info</a>  
+			</div>          
         </div>
 	
 	
-		<div id="who-can-join" class="my-md-5">
+		<div id="who-can-join" class="my-5">
 			<div class="section-header container">
 				<h2 class="text-deep-teal">Who Can Join?</h2>   
 				<p>Members are required to possess the following qualifications:</p>
@@ -51,16 +52,16 @@
 				</div>
 			</div>
 		</div>
-		<div class="row bg-mint-green" id="membership-benefits">
-			<div class="section-header container my-md-5">
+		<div class="bg-mint-green py-5" id="membership-benefits">
+			<div class="section-header container">
 				<h2 class="text-deep-teal">Membership Benefits</h2>   
 				<p>This organization is committed to the growth and development of the digital service industry. Members in the IDC have access to business resources, mentorship from companies and individuals with similar experiences, exclusive and impactful networking, education, and advocacy opportunities, and business development resources.</p>
 			</div>
 		</div>
-		<div class="row" id="education">
+		<div id="education">
             <div class="section-details container">
-				<div class="row my-md-5">
-	                <div class="col-md-6">      
+				<div class="row py-5">
+	                <div class="col-md-6 my-2">      
 						<h3 class="text-deep-teal">Education</h3>             
 	                    <p>IDC offers varied education opportunities including: </p>
 	                    <ul>
@@ -70,19 +71,19 @@
 	                        <li>Opportunities to speak on behalf of the IDC and its community at trade shows, meet-ups, and other external events</li>
 	                    </ul>
 					</div>
-					<div class="col-md-6 benefit-photo"> 
+					<div class="col-md-6 my-2 benefit-photo"> 
 						<img src="@/assets/education.jpg" alt="Person on webinar."/>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="row bg-light-gray" id="events">
+		<div class="bg-light-gray py-5" id="events">
             <div class="section-details container">
-				<div class="row my-md-5">
-	                <div class="col-md-6 benefit-photo">                
+				<div class="row">
+	                <div class="col-md-6 benefit-photo my-2">                
 	                    <img src="@/assets/networking-events.jpg" alt="Group of people networking."/>
 					</div>
-					<div class="col-md-6"> 
+					<div class="col-md-6 order-first order-md-1 my-2"> 
 						<h3 class="text-deep-teal">Events</h3>   
 	                    <p>Networking is the lifeblood of the digital service community and we provides many events - both in-person and online to connect and reinforce relationships, including:</p>
 	                    <ul>
@@ -96,10 +97,10 @@
 				</div>
 			</div>
 		</div>
-		<div class="row" id="policy-and-advocacy">
+		<div class="py-5" id="policy-and-advocacy">
             <div class="section-details container">
-				<div class="row my-md-5">
-	                <div class="col-md-6"> 
+				<div class="row">
+	                <div class="col-md-6 my-2"> 
 						<h3 class="text-deep-teal">Policy and Advocacy</h3>   
 	                    <p>The IRS Digital Coalition champions the interests of its members in the face of governmental legislation, policies, and regulations impacting our domain: </p>
 	                    <ul>
@@ -109,19 +110,19 @@
 	                        <li>Undertakes initiatives to amplify opportunities for socio-economic groups and marginalized talent in the tech realm, in line with the IRS's focus on enhancing taxpayer services and experiences.</li>
 	                    </ul>               	                  
 					</div>
-					<div class="col-md-6 benefit-photo"> 
+					<div class="col-md-6 benefit-photo my-2"> 
 						<img src="@/assets/capitol.jpg" alt="Group of people networking."/>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="row bg-light-gray" id="intel-and-bd">
+		<div class="bg-light-gray py-5" id="intel-and-bd">
             <div class="section-details container">
-				<div class="row my-md-5">
-	                <div class="col-md-6 benefit-photo">                
+				<div class="row">
+	                <div class="col-md-6 benefit-photo my-2">                
 	                    <img src="@/assets/business-development.jpg" alt="Two men looking at something on a computer."/>
 					</div>
-					<div class="col-md-6"> 
+					<div class="col-md-6 order-first order-md-1 my-2"> 
 						<h3 class="text-deep-teal">Market Intelligence & Business Development</h3>   
 	                    <b>Market Intelligence and Business Development:</b>
 	                    <p>Conducts activities to understand and grow the industry opportunities for digital service firms: </p>
@@ -136,7 +137,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="row my-md-5" id="how-to-join">
+		<div class="my-5" id="how-to-join">
 			<div class="section-header container">
 				<h2 class="text-deep-teal">How to Join</h2>
 				<p>Each existing charter member firm can evaluate and agree to sponsor one prospective new member firm per year. The charter member firm is responsible for mentoring and guiding the sponsored firm in the mission and values of the coalition. Sponsored firms must provide at least two references and pass a review by the coalition onboarding panel.</p>   
